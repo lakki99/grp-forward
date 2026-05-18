8bf532fd22ffb3bea07fdaab49862a4904e84894
Lakki reddy <pulka143341@gmail.com>
2026-05-18 15:52:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,22 @@
+import os
 from pyrogram import Client, filters
 
-# API details
-API_ID = 12345678
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-# IDs
-SOURCE_GROUP = -1002294325631   # Group ID
-TARGET_CHANNEL = -1003775458189  # Channel ID
+SOURCE_GROUP = int(os.environ.get("-1002294325631"))
+TARGET_CHANNEL = int(os.environ.get("-1003775458189"))
 
 app = Client(
-    "forward_bot",
+    "bot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN
 )
 
-# Only documents/files
-@app.on_message(
-    filters.chat(SOURCE_GROUP) &
-    filters.document
-)
-async def forward_docs(client, message):
-    try:
-        await message.forward(TARGET_CHANNEL)
-        print("Document forwarded")
-    except Exception as e:
-        print(e)
+@app.on_message(filters.chat(SOURCE_GROUP) & filters.document)
+async def forward(client, message):
+    await message.forward(TARGET_CHANNEL)
 
 app.run()
